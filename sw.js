3b79316c748633da859feffa3026cd0fe0ba0574
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-07-22 00:58:31 +1200

    github sw copy paste

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
 const version = "3.2";
 const whatchanged = 'keyboard shortcuts added firebase auth and firestore fixed display none, added closeNav() '
 const cacheName = 'goalsappcache';
-const contentToCache = [
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+
+const cacheFiles = // ['/', '/about-me/', '/projects/', '/offline/'];
+//const contentToCache = 
+[
   '/',
   '/index.html',
   '/arrow.png',
@@ -11,22 +16,89 @@ const contentToCache = [
   '/style.css',
   '/side-nav-style.css',
 ];
-self.addEventListener('activate', e => self.clients.claim());
+// self.addEventListener('activate', e => self.clients.claim());
 
-// the event handler for the install event 
-// typically used to cache assets
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll(contentToCache))
-  );
-});
+// // the event handler for the install event 
+// // typically used to cache assets
+// self.addEventListener('install', e => {
+//   e.waitUntil(
+//     caches.open(cacheName)
+//     .then(cache => cache.addAll(contentToCache))
+//   );
+// });
 
-// network first cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+// // network first cache
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request).catch(function() {
+//       return caches.match(event.request);
+//     })
+//   );
+// });
+
+// the cache version gets updated every time there is a new deployment
+
+
+// these are the routes we are going to cache for offline support
+
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
+  )
+);
+
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || cache.match('/offline/'))
+    );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
   );
+  evt.waitUntil(update(evt.request));
 });
