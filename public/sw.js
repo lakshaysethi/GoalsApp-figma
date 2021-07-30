63c6a303d1fee7b6cff1ab4d4623838597a76b8f
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-07-30 13:41:36 +1200

    moved continue button

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const version = "3.3.5";
-const whatchanged = 'added keyboardshorts for navigation  added go deeper button and functionality'
+const version = "3.3.6";
+const whatchanged = 'moved continue button up added keyboardshorts for navigation  added go deeper button and functionality'
 const cacheName = 'goalsappcache';
 const contentToCache = [
   '/',
