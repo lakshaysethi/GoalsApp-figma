d44e495e3fe085c2eccc4fcdc2f60a76f562d085
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2021-12-07 10:37:21 +1300

    Update main.py

--- cli/main.py
+++ cli/main.py
@@ -1,11 +1,20 @@
-from typing import Optional
+# from typing import Optional
 from fastapi import FastAPI
+from tinydb import TinyDB, Query
+import uvicorn
+db = TinyDB('db.json')
+
+# db.insert({'type': 'apple', 'count': 7})
+
+# https://tinydb.readthedocs.io/en/latest/getting-started.html
+
+# https://fastapi.tiangolo.com/tutorial/first-steps/
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"text": "nothing at root "}
 
 
 @app.get("/goals/{item_id}")
@@ -18,3 +27,13 @@ def read_all():
     array = []
     return array
 
+
+@app.post("/goals/new")
+def create_new(name:str):
+    goal = {}
+    # goal.
+    # db.insert()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
