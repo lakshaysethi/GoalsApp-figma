51d088a01a2ed973746ec0ece726a6d25f89fbf2
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2021-12-07 10:17:52 +1300

    updated

--- cli/main.py
+++ cli/main.py
@@ -9,12 +9,12 @@ def read_root():
 
 
 @app.get("/goals/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return {"item_id": item_id, "more": "q"}
 
 
 @app.get("/goals/")
 def read_all():
-    aray = []
+    array = []
     return array
 
