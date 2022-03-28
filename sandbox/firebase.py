5fc5fc656ba6ceb138fa95fe0a87767e1a60c1d9
Lakshay Sethi <lakshay@plantme.io>
2022-03-29 11:35:37 +1300

    updated

--- sandbox/firebase.py
+++ sandbox/firebase.py
@@ -3,15 +3,14 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
-cred = credentials.Certificate('./sandbox/tensile-rite-345521-ee8201120995.json')
+cred = credentials.Certificate('/home/ubuntu/lakshay/GoalsApp-figma/sandbox/tensile-rite-345521-ee8201120995.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-
-doc_ref = db.collection(u'users').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
+lsgs = db.collection('users').document("lakshay")
+lsgs.set({
+    'goals': [{"goal":"mg"}]
 })
+a = lsgs.get()
+print(a.to_dict())
