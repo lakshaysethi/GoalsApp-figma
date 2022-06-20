ee63788968b7c1420211c843c66fc7e5c6ab6cae
lakshaysethi <lakshay.sethi@lakshay.sethi>
2022-06-20 19:34:30 +1200

    revert commit on main

--- public/definations.js
+++ public/definations.js
@@ -12,8 +12,7 @@ class Goal{
     notes = ''
 }
 
-// let syncallowed = true //its 20k per day not month bro! 
-let syncallowed = false
+let syncallowed = true //its 20k per day not month bro! 
 
 let main_goals_array = []
 const login_button = document.getElementById('login-with-google')
